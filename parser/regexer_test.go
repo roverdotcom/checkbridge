958a584d776f5dbf5be94c20f0a787a3aa989f0e
Aiden Scandella <git@sca.ndella.com>
2020-02-10 15:45:21 -0800

    Add another regex test

--- parser/regexer_test.go
+++ parser/regexer_test.go
@@ -1,14 +1,15 @@
 package parser_test
 
 import (
-	"bufio"
 	"bytes"
 	"errors"
 	"regexp"
+	"strconv"
 	"testing"
 
 	"github.com/roverdotcom/checkbridge/parser"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var testRegexp = regexp.MustCompile("(.*)")
@@ -21,10 +22,32 @@ func TestRegexerRun_WithError(t *testing.T) {
 		return nil, errors.New("whoops")
 	}
 
-	r := parser.NewRegexer(testRegexp, extractWithError, bufio.NewReader(bytes.NewBufferString("test")))
+	r := parser.NewRegexer(testRegexp, extractWithError, bytes.NewBufferString("test"))
 
 	_, err := r.Run()
 
 	assert.NoError(t, err)
 	assert.True(t, called)
+}
+
+func TestRegexerRun_SomeAnnotationsOK(t *testing.T) {
+	regex := regexp.MustCompile("^(.*):([0-9]+): (.*)")
+	extractor := func(matches []string) ([]parser.Annotation, error) {
+		line, err := strconv.Atoi(matches[2])
+		require.NoError(t, err)
+
+		return []parser.Annotation{{
+			Path: matches[1],
+			Line: line,
+		}}, nil
+	}
+
+	r := parser.NewRegexer(regex, extractor, bytes.NewBufferString(`
+foo/bar.go:123: message
+`))
+
+	result, err := r.Run()
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(result.Annotations))
+	assert.Equal(t, "foo/bar.go", result.Annotations[0].Path)
 }
