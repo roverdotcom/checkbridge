3ae9cced0383985aca68f9d8a53ee73362efcf07
Aiden Scandella <aiden.scandella@rover.com>
2020-02-10 13:06:52 -0800

    Add golint parser (#2)
    * Add golint stub
    
    * Add to path
    
    * Flesh out golint
    
    * Fix tests
    
    * Add comment to Execute

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +9,30 @@ var rootCmd = &cobra.Command{
 	Use:   "checkbridge",
 	Short: "Checkbridge automates creating GitHub checks for CI",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, world")
+		configureLogging(cmd)
+		logrus.Info("Hello, world")
 	},
 }
 
-// Execute is the entrypoint of the CLI application
+func configureLogging(cmd *cobra.Command) {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableTimestamp: true,
+	})
+	if isVerbose, err := cmd.Flags().GetBool("verbose"); err != nil {
+		logrus.WithError(err).Error("Unable to read verbosity")
+	} else if isVerbose {
+		logrus.SetLevel(logrus.DebugLevel)
+		logrus.Debug("Enabled verbose logging")
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "verbose output")
+
+	rootCmd.AddCommand(golintCmd)
+}
+
+// Execute is the entrypoint of the application
 func Execute() error {
 	return rootCmd.Execute()
 }
---
+++ parser/golint_test.go
@@ -0,0 +1,39 @@
+package parser_test
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+
+	"github.com/roverdotcom/checkbridge/parser"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func makeLinter(input string) parser.Golinter {
+	return parser.NewGolinter(bufio.NewReader(bytes.NewBufferString(input)))
+}
+
+func TestGolinter_ValidMatches(t *testing.T) {
+	assert := assert.New(t)
+
+	linter := makeLinter(`
+cmd/root.go:35:1: exported function Execute should have comment or be unexported
+not a valid line
+	`)
+
+	results, err := linter.Run()
+	require.NoError(t, err, "Error running linter")
+	assert.Equal(1, len(results.Annotations))
+	a := results.Annotations[0]
+	assert.Equal("cmd/root.go", a.Path)
+	assert.Equal(35, a.Line)
+	assert.Equal(parser.LevelWarning, a.Level)
+}
+
+func TestGolinter_InvalidColumns(t *testing.T) {
+	linter := makeLinter(`foo/bar.go:abcd:1234: foo bar`)
+	results, err := linter.Run()
+	require.NoError(t, err, "Error running linter")
+	assert.Equal(t, 0, len(results.Annotations))
+}
