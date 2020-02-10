6994be42e82800ed09c5119b169066cb53984a2a
Aiden Scandella <aiden.scandella@rover.com>
2020-02-10 13:23:00 -0800

    Add lint workflow (#4)
    * Add lint workflow
    
    * Trigger lint
    
    * Fix name
    
    * Update job name
    
    * Only run test on push
    
    * Fix gofmt
    
    * Only run golint during lint phase

--- cmd/golint.go
+++ cmd/golint.go
@@ -18,18 +18,20 @@ var golintCmd = &cobra.Command{
 		logrus.Debug("Parsing golint results")
 
 		scanner := bufio.NewReader(os.Stdin)
-		glParser := parser.NewGolinter(scanner)
-		results, err := glParser.Run()
+		gl := parser.NewGolinter(scanner)
+		results, err := gl.Run()
 		if err != nil {
 			logrus.WithError(err).Error("Error parsing golint results")
 			return
 		}
+
 		if len(results.Annotations) == 0 {
-			logrus.Debug("No annotations reported from golint parsing")
+			logrus.Info("No violations reported from golint")
 			return
 		}
 
 		logrus.Infof("Got results: %+v", results)
+		// TODO report the annotations to GitHub checks API
 		os.Exit(1)
 	},
 }
