b8a8af972299c14578cdc9046333194eb6aaba27
Aiden Scandella <aiden.scandella@rover.com>
2020-02-11 14:15:50 -0800

    Refactor command runner and add tests

---
+++ cmd/runner_test.go
@@ -0,0 +1,118 @@
+// MIT License
+//
+// Copyright (c) 2020 Rover.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package cmd
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/roverdotcom/checkbridge/github"
+	"github.com/roverdotcom/checkbridge/parser"
+	flag "github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+)
+
+type stubClient struct {
+	reportedCheck *github.CheckRun
+	err           *error
+}
+
+func (s *stubClient) CreateCheck(check github.CheckRun) error {
+	s.reportedCheck = &check
+	if s.err != nil {
+		return *s.err
+	}
+	return nil
+}
+
+func TestAPIClient_NoToken(t *testing.T) {
+	r := runner{
+		env: func(string) string { return "" },
+	}
+
+	_, err := r.apiClient(repo{})
+	assert.Error(t, err)
+}
+
+func TestAPIClient_ValidToken(t *testing.T) {
+	r := runner{
+		env: func(string) string { return "token" },
+	}
+
+	_, err := r.apiClient(repo{})
+	assert.NoError(t, err)
+}
+
+func TestReportResults_NoViolations(t *testing.T) {
+	api := &stubClient{}
+	result := parser.Result{}
+	r := runner{}
+	code := r.reportResults(github.CheckRun{}, result, api)
+
+	assert.Equal(t, code, 0)
+	assert.Equal(t, github.CheckConclusionSuccess, api.reportedCheck.Conclusion)
+}
+
+func TestReportResults_WithViolations(t *testing.T) {
+	flags := &flag.FlagSet{}
+	flags.BoolP("exit-zero", "z", false, "usage")
+	api := &stubClient{}
+	result := parser.Result{
+		Annotations: []parser.Annotation{{
+			Path: "main.go",
+		}},
+	}
+	r := runner{flags: flags}
+	code := r.reportResults(github.CheckRun{}, result, api)
+
+	assert.Equal(t, code, 1)
+	assert.Equal(t, github.CheckConclusionFailure, api.reportedCheck.Conclusion)
+}
+
+func TestReportResults_WithViolations_ExitZero(t *testing.T) {
+	flags := &flag.FlagSet{}
+	flags.BoolP("exit-zero", "z", true, "usage")
+	api := &stubClient{}
+	result := parser.Result{
+		Annotations: []parser.Annotation{{
+			Path: "main.go",
+		}},
+	}
+	r := runner{flags: flags}
+	code := r.reportResults(github.CheckRun{}, result, api)
+
+	assert.Equal(t, code, 0)
+	assert.Equal(t, github.CheckConclusionFailure, api.reportedCheck.Conclusion)
+}
+
+func TestReportResults_GitHubError(t *testing.T) {
+	err := errors.New("unicorns")
+	api := &stubClient{
+		err: &err,
+	}
+	result := parser.Result{}
+	r := runner{}
+	code := r.reportResults(github.CheckRun{}, result, api)
+
+	assert.Equal(t, code, 5)
+}
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
 )
